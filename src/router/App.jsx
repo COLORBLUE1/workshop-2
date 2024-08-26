a6642d43814bde094c02fbc7f28fe65367c2b10f
abel-git <camilosol123@gmail.com>
2024-08-26 09:22:13 -0500

    🎉 init:

--- src/router/App.jsx
+++ src/router/App.jsx
@@ -1,14 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}>
             <Route index element={<Welcome />} />
@@ -17,7 +14,7 @@ function App() {
           </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
     </>
   );
 }
