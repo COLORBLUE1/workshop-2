10e160cb7c164bd0f55d5dd879451ba4ad7b526b
unknown <andresep.30@gmail.com>
2024-08-26 10:16:54 -0500

    archivos Login y CreateAcount creados

---
+++ src/assets/Componentes/CreateAcount.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateAcount = () => {
+  return (
+    <>
+      aqui va el create acount
+    </>
+  )
+}
+
+export default CreateAcount
---
+++ src/assets/Componentes/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+      aqui va el login
+    </>
+  )
+}
+
+export default Login
